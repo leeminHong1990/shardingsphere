99b59ed8c75eeb986e4c53c66682adebf59d1b74
honglimin <693749857@qq.com>
2020-05-26 14:47:11 +0800

    新增同一数据库分表demo

---
+++ sub-table/src/main/java/com/min/subtable/SubTableApplication.java
@@ -0,0 +1,14 @@
+package com.min.subtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author min
+ */
+@SpringBootApplication
+public class SubTableApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SubTableApplication.class, args);
+    }
+}
---
+++ sub-table/src/main/java/com/min/subtable/config/AppConfig.java
@@ -0,0 +1,13 @@
+package com.min.subtable.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author min
+ */
+@Slf4j
+@Configuration
+public class AppConfig {
+
+}
